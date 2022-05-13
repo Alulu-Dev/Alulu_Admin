c6a73855b6844e212fc756b48d5493ef172bb883
RediateB <50769628+RediateB@users.noreply.github.com>
2022-05-13 12:35:56 +0300

    Sidebar edited,Login integrated

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,67 +1,93 @@
-import "./sidebar.scss"
-import { DashboardFilled, UserOutlined,DollarOutlined,PullRequestOutlined,AreaChartOutlined,QuestionCircleOutlined,SettingOutlined,ProfileOutlined, LogoutOutlined, BellOutlined } from '@ant-design/icons';
-import Navbar from "../Navbar/Navbar"
-import { Link } from 'react-router-dom';
-const Sidebar = () =>{
-    return (
-        
-        <div className="Sidebar">
-            
-            <div className="top"><span className="logo">Alulu</span></div>
-            <hr />
-            <div className="center">
-                <ul>
-                    <p className="title">Main</p>
-                    <li>
-                    <DashboardFilled className="icon"/>
-                        {/* <span>Dashboard</span> */}
-                    <span><Link to="/" className="btn">Dashboard</Link></span>
-                    </li>
-                    <p className="title">Lists</p>
-                    <li>
-                    <UserOutlined className="icon" />
-                    <span><Link to="/users" className="btn">Users</Link></span>
-                    </li>
-                    <li>
-                    <DollarOutlined className="icon"/>
-                    <span><Link to="/fraud" className="btn">Fraud</Link></span>
-                    </li>
-                    <li>
-                    <PullRequestOutlined className="icon"/>
-                    <span><Link to="/request" className="btn">Request</Link></span>
-                    </li>
-                    <li>
-                    <AreaChartOutlined className="icon"/>
-                    <span><Link to="/stats" className="btn">Stats</Link></span>
-                    </li>
-                    <li>
-                    <BellOutlined className="icon"/>
-                    <span><Link to="/notification" className="btn">Notification</Link></span>
-                    </li>
-                    <p className="title">Services</p>
+import "./sidebar.scss";
+import {
+  DashboardFilled,
+  UserOutlined,
+  DollarOutlined,
+  PullRequestOutlined,
+  AreaChartOutlined,
+  QuestionCircleOutlined,
+  SettingOutlined,
+  ProfileOutlined,
+  LogoutOutlined,
+  BellOutlined,
+} from "@ant-design/icons";
+import Navbar from "../Navbar/Navbar";
+import { Link } from "react-router-dom";
+const Sidebar = () => {
+  const logout = () => {
+    localStorage.removeItem("Token");
+  };
+  return (
+    <div className="Sidebar">
+      <div className="top">
+        <span className="logo">Alulu</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">Main</p>
+          <li>
+            <DashboardFilled className="icon" />
+            {/* <span>Dashboard</span> */}
+            <span>
+              <Link to="/" className="btn">
+                Dashboard
+              </Link>
+            </span>
+          </li>
+          <p className="title">Lists</p>
+          <li>
+            <UserOutlined className="icon" />
+            <span>
+              <Link to="/users" className="btn">
+                Users
+              </Link>
+            </span>
+          </li>
+          <li>
+            <DollarOutlined className="icon" />
+            <span>
+              <Link to="/fraud" className="btn">
+                Fraud
+              </Link>
+            </span>
+          </li>
+          <li>
+            <PullRequestOutlined className="icon" />
+            <span>
+              <Link to="/request" className="btn">
+                Request
+              </Link>
+            </span>
+          </li>
+          
+          
+          <p className="title">Services</p>
 
-                    <li>
-                    <SettingOutlined className="icon"/>
-                    <span><Link to="/settings" className="btn">Settings</Link></span>
-                        
-                    </li>
-                    <li>
-                    <QuestionCircleOutlined className="icon"/>
-                    <span><Link to="/faq" className="btn">FAQ</Link></span>
-                       
-                    </li>
-                    <p className="title">User</p>
-                   
-                    <li>
-                    <LogoutOutlined className="icon"/>
-                    <span><Link to="/login" className="btn">LogOut</Link></span>
-                    </li>   
-                </ul>
-            </div>
-            <div className="bottom"></div>
+          
+          <li>
+            <QuestionCircleOutlined className="icon" />
+            <span>
+              <Link to="/faq" className="btn">
+                FAQ
+              </Link>
+            </span>
+          </li>
+          <p className="title">User</p>
 
-        </div>
-    )
-}
+          <li>
+            <LogoutOutlined className="icon" />
+            <span>
+              <Link to="/login" onClick={logout} className="btn">
+                LogOut
+              </Link>
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
