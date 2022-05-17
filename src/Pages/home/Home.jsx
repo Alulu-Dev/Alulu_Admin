10fac9dfdf62a1d0db70db1c3f7e1c6175323b6e
RediateB <50769628+RediateB@users.noreply.github.com>
2022-05-17 12:36:47 +0300

    home page integrated

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,28 +1,58 @@
-import React from 'react';
-import Sidebar from '../../components/Sidebar/Sidebar';
-import "./home.scss"
-import Navbar from "../../components/Navbar/Navbar"
-import Widget from '../../components/widget/Widget';
-import Chart from '../../components/chart/Chart';
-import { userData } from '../../dummyData'; 
+import React, { useState, useEffect } from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import "./home.scss";
+import Navbar from "../../components/Navbar/Navbar";
+import Widget from "../../components/widget/Widget";
+import Chart from "../../components/chart/Chart";
+import { userData } from "../../dummyData";
 
 const Home = () => {
-    return (
-        
-        <div className='home'>
-            <Sidebar/>
-            <div className="homeContainer">
-                 <Navbar/> 
-                 <div className="widgets">
-                     <Widget type="user"/>
-                     <Widget type="fraud"/>
-                     <Widget type="request"/>
-                     <Widget type="receiptUpload"/>
-                 </div>
-                 <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
-            </div>
-        </div>
-    )
-}
+  const [systemSummary, setSystemSummary] = useState([{}]);
 
-export default Home
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/v2/summary/system/", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer " + localStorage.getItem("Token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setSystemSummary(data));
+  }, []);
+
+  const monthlyData = systemSummary["monthly Registry"];
+
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" incoming_data={systemSummary["Total Users"]} />
+          <Widget
+            type="fraud"
+            incoming_data={systemSummary["Total Fraud Case"]}
+          />
+          <Widget
+            type="request"
+            incoming_data={systemSummary["Total Requests"]}
+          />
+          <Widget
+            type="receiptUpload"
+            incoming_data={systemSummary["Total Receipts"]}
+          />
+        </div>
+        <Chart
+          // data={userData}
+          data={monthlyData}
+          title="User Analytics"
+          grid
+          dataKey="Active User"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
