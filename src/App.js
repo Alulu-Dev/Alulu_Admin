f0e4f9af9ff39a32cf08610e40ca6a3a4231244d
RediateB <50769628+RediateB@users.noreply.github.com>
2022-05-25 22:39:20 +0300

    Widget edited and route edited

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 // import logo from './logo.svg';
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route
+import React, { useState, useEffect } from "react";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Router, Route, Switch, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar/Sidebar";
+import "./App.css";
+import Home from "./Pages/home/Home";
+import Login from "./Pages/Login/Login";
+import Single from "./Pages/single/Single";
+import New from "./Pages/new/New";
+import List from "./Pages/list/List";
+import Users from "./Pages/userList/UserList";
+import Fraud from "./Pages/fraud/Fraud";
+import Request from "./Pages/request/Request";
+import Matcher from "./Pages/matcher/Matcher";
 
-} from "react-router-dom";
-import Navbar from './components/Navbar/Navbar';
-// import { BrowserRouter, Router,Route, Switch, Routes } from 'react-router-dom';
-import Sidebar from './components/Sidebar/Sidebar';
-import './App.css';
-import Home from "./Pages/home/Home"
-import Login from './Pages/Login/Login'
-import Single from './Pages/single/Single'
-import New from './Pages/new/New'
-import List from './Pages/list/List'
-import Users from './Pages/userList/UserList';
-import Fraud from './Pages/fraud/Fraud'
-import Request from './Pages/request/Request'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  return (
+  // const [initialData, setInitialData] = useState([{}])
 
+  // useEffect(()=> {
+  //   fetch('http://127.0.0.1:5000/api',{
+  //     headers:{
+  //     'Content-Type': 'application/json',
+  //     'Accept': 'application/json'}
+  //   }).then(
+  //     response => response.json()
+  //   ).then(data => setInitialData(data))
+  // },[]);
+
+  return (
+    //  <div className='App'>
+    //    <h1>{initialData.title}</h1>
+    //  </div>
     // <Router>
     // < Navbar/>
     // <Sidebar/>
@@ -30,26 +45,22 @@ function App() {
     //   </Switch>
     // </Router>
 
-
-    <div className='App'>
-
+    <div className="App">
       <BrowserRouter>
         <Routes>
-
           <Route exact path="/" element={<Home />}></Route>
           <Route path="login" element={<Login />}></Route>
-          <Route path="users" element={<Users />}>
-            
-            <Route index element={<List />}></Route>
-            <Route path='userId' element={<Single />} />
-            <Route path='new' element={<New />} />
-          </Route>
-          <Route exact path="/fraud" element={<Fraud/>}></Route>
-          <Route exact path="/request" element={<Request />}></Route>
-          
-          <Route path="/" element={<Home />}></Route>
-          
 
+          <Route path="users" element={<Users />}>
+            <Route index element={<List />}></Route>
+            <Route path="userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+          <Route exact path="/fraud" element={<Fraud />}></Route>
+          <Route exact path="/request" element={<Request />}></Route>
+          <Route exact path="/matcher" element={<Matcher />}></Route>
+
+          <Route path="/" element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,75 +1,71 @@
-import React from 'react';
-import "./widget.scss"
-import { UpOutlined,UserOutlined,MoneyCollectOutlined,PullRequestOutlined,ReconciliationOutlined } from '@ant-design/icons';
+import React from "react";
+import "./widget.scss";
+import {
+  UpOutlined,
+  UserOutlined,
+  MoneyCollectOutlined,
+  PullRequestOutlined,
+  ReconciliationOutlined,
+} from "@ant-design/icons";
 
+const Widget = ({ type, incoming_data }) => {
+  //Tempo
+ 
+  let data;
 
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        amount: incoming_data,
+        isMoney: false,
+        icon: <UserOutlined className="icon" />,
+      };
+      break;
+    case "fraud":
+      data = {
+        title: "FRAUD",
+        amount: incoming_data,
+        isMoney: false,
+        icon: <MoneyCollectOutlined className="icon" />,
+      };
+      break;
+    case "request":
+      data = {
+        title: "REQUEST",
+        amount: incoming_data,
+        isMoney: false,
+        icon: <PullRequestOutlined className="icon" />,
+      };
+      break;
+    case "receiptUpload":
+      data = {
+        title: "RECEIPTS",
+        amount: incoming_data,
+        isMoney: false,
+        icon: <ReconciliationOutlined className="icon" />,
+      };
 
-const Widget = ({type}) => {
-
-//Tempo
-const amount =1000
-const diff = 20
-
-
-    let data;
-
-        switch(type){
-            case "user":
-                data={
-                    title:"USERS",
-                    isMoney:false,
-                    link:"See all users",
-                    icon:<UserOutlined className='icon'/>
-                };
-                break;
-                case "fraud":
-                    data={
-                    title:"FRAUD",
-                    isMoney:false,
-                    link:"See all fraud cases",
-                    icon:<MoneyCollectOutlined className='icon'/>
-                };
-                break;
-                case "request":
-                    data={
-                    title:"REQUEST",
-                    isMoney:false,
-                    link:"See all requests",
-                    icon:<PullRequestOutlined className='icon'/>
-                };
-                break;
-                case "receiptUpload":
-                    data={
-                    title:"RECEIPTS",
-                    isMoney:false,
-                    link:"See uploaded receipts",
-                    icon:<ReconciliationOutlined className='icon'/>
-                };
-            
-                break;
-                default:
-                    break;
-
-        }
-    return (
-        <div className="widget">
-            <div className="left">
-                <span className='title'>{data.title}</span>
-                <span className='counter'>{amount}</span>
-                <span className='link'>{data.link}</span>
-
-            </div>
-            <div className="right">
-                <div className="percentage negative">
-                <UpOutlined />
-                    {diff}%
-                </div>
-                {data.icon}
-                
-            </div>
+      break;
+    default:
+      break;
+  }
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">{data.amount}</span>
+        <span className="link">{data.link}</span>
+      </div>
+      <div className="right">
+        <div className="percentage negative">
+          
+         
         </div>
-    )
+        {data.icon}
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default Widget
+export default Widget;
